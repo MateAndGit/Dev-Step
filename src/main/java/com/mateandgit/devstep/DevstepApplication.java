1f19c3cad6d8f98dce4494822bc4472ebb790c7f
USER <sola201@naver.com>
2026-02-05 16:38:25 -0300

    feat:Initial  Spring Boot & and Setup Docker with PostgreSQL(#1)

---
+++ src/main/java/com/mateandgit/devstep/DevstepApplication.java
@@ -0,0 +1,13 @@
+package com.mateandgit.devstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateandgit/devstep/DevstepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateandgit.devstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
