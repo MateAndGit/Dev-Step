5d3e169885e225bdb05f2139f230fb4ea7fa0bc1
USER <sola201@naver.com>
2026-02-05 20:51:08 -0300

    fix: remove legacy health check

--- backend/src/main/java/com/mateandgit/devstep/repository/HealthCheckRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mateandgit.devstep.repository;
-
-import com.mateandgit.devstep.entity.HealthCheck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthCheckRepository extends JpaRepository<HealthCheck, Long> {
-}
