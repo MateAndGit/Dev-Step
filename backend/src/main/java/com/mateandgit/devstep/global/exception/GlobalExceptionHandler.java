208eb20b5728637fe0b556dec17857aeaee94800
USER <sola201@naver.com>
2026-02-11 20:06:33 -0300

    refactor: #6 add logging to GlobalExceptionHandler

--- backend/src/main/java/com/mateandgit/devstep/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mateandgit/devstep/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,21 @@
 package com.mateandgit.devstep.global.exception;
 
 import com.mateandgit.devstep.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         final ApiResponse response = ApiResponse.fail(ErrorCode.INVALID_INPUT_VALUE);
+        log.error("handleMethodArgumentNotValidException: ", e);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
@@ -20,12 +23,14 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ApiResponse> handleBusinessException(final BusinessException e) {
         final ErrorCode errorCode = e.getErrorCode();
         final ApiResponse response = ApiResponse.fail(errorCode);
+        log.error("handleBusinessException: ", e);
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ApiResponse> handleException(Exception e) {
         final ApiResponse response = ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
+        log.error("handleException: ", e);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
